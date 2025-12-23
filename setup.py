584e865d333994f18b7a5d82b1edbafa42fb9c08
purushotham.r <118825091+purushothamr01@users.noreply.github.com>
2025-12-23 09:48:02 +0530

    setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,42 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name="reconx",
-    version="1.0",
+    version="1.0.0",
+    author="Purushotham R",
+    author_email="your_email@example.com",
+    description="A personal reconnaissance framework for bug bounty",
+    long_description=open("README.md", "r", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/yourusername/reconx",
+    packages=find_packages(),
     py_modules=["reconx"],
-    install_requires=[],
+    python_requires=">=3.8",
+    install_requires=[
+        "requests",
+        "httpx",
+        "aiohttp",
+        "asyncio",
+        "pyjsparser",
+        "beautifulsoup4",
+        "slack_sdk",
+        "discord-webhook",
+        "colorama",
+        "termcolor",
+        "rich",
+        "pyyaml",
+        "tqdm",
+        "packaging",
+        "regex"
+    ],
     entry_points={
         "console_scripts": [
-            "reconx=reconx:main"
-        ]
+            "reconx = reconx:main",
+        ],
     },
-    author="Purushotham R",
-    description="Bug bounty focused reconnaissance framework",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
 )
